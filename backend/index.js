c1a3872495b70cb8f48d1865669ec860344f40b4
JUDHI3134 <judhistirbehera241@gmail.com>
2024-03-23 19:20:43 +0530

    changes for deploy

--- backend/index.js
+++ backend/index.js
@@ -2,24 +2,32 @@ const express = require("express");
 const cors = require("cors");
 const { connect } = require("mongoose");
 require("dotenv").config();
-const upload = require("express-fileupload")
+const upload = require("express-fileupload");
 
-const userRoutes = require("./routes/userRoutes")
-const postRoutes = require("./routes/postRoutes")
-const {notFound, errorHandler} = require("./middlewares/errorMiddleware")
+const path = require("path");
+__dirname = path.resolve();
+
+const userRoutes = require("./routes/userRoutes");
+const postRoutes = require("./routes/postRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(upload());
-app.use("/uploads",express.static(__dirname + "/uploads"));
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
+
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend",'build','index.html'));
+});
 
 connect(process.env.MONGO_URI).then(
   app.listen(process.env.PORT || 5000, () => {
